7c4ea1e439fb6069d94f3877e8ffe62b46946f0b
Ninjseal <b.marius10@yahoo.com>
2020-06-19 16:22:31 +0300

    Lots of good changes.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,6 +3,10 @@ module ApplicationHelper
     controller_name.inquiry
   end
 
+  def action_inquiry
+    action_name.inquiry
+  end
+
   def nav_item_class(active = false)
     "nav-item#{active ? ' active' : ''}"
   end
