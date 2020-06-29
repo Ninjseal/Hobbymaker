5b7c4b516ad2d6bb29c71a3e5e81a32acb33fbff
Ninjseal <b.marius10@yahoo.com>
2020-06-29 19:03:12 +0300

    Added profile photo to users.

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,16 +24,25 @@ class User < ApplicationRecord
   has_many :following_follows, foreign_key: :follower_id, class_name: 'Follow'
   has_many :following, through: :following_follows, source: :following
 
+  before_create :set_default_profile_photo
   after_create :assign_default_role
 
   enum gender: { male: 0, female: 1, other: 2 }
 
-  def assign_default_role
-    self.add_role :user if self.roles.blank?
-  end
+  has_one_attached :profile_photo
 
   def direct_chats
     TwoUsersChat.where(["user1_id = :user_id OR user2_id = :user_id", { user_id: self.id }])
   end
+
+  private
+
+    def assign_default_role
+      self.add_role :user if self.roles.blank?
+    end
+
+    def set_default_profile_photo
+      self.profile_photo.attach(io: File.open(Rails.root.join('app/assets/images/default-profile-photo.png')), filename: 'default-profile-photo.png', content_type: 'image/png')
+    end
 
 end
